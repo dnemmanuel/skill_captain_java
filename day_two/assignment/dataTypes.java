3f9de694787f456dfd625655972bbd6e6db5f90a
dnemmanuel <dinnelemmanuel@gmail.com>
2023-09-03 23:51:34 -0400

    day_two assingment

---
+++ day_two/assignment/dataTypes.java
@@ -0,0 +1,23 @@
+package day_two.assignment;
+
+public class dataTypes {
+    public static void main(String[] args) {
+        byte byteVal = 5;
+        short shortVal = 15;
+        int intVal = 500;
+        long longVal = 15500L;
+        float floatVal = 5.56f;
+        double doubleVal = 3.36678d;
+        char charVal = 'a';
+        boolean booleanVal = false;
+
+        System.out.println("My byte value is " + byteVal);
+        System.out.println("My short value is " + shortVal);
+        System.out.println("My integer value is " + intVal);
+        System.out.println("My long value is " + longVal);
+        System.out.println("My float value is " + floatVal);
+        System.out.println("My double value is " + doubleVal);
+        System.out.println("My character value is " + charVal);
+        System.out.println("My boolean value is " + booleanVal);
+    }
+}
