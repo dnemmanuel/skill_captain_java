8323a85456044a5436c9adf9457d5a79da8ab4c5
dnemmanuel <dinnelemmanuel@gmail.com>
2023-09-05 14:27:49 -0400

    assignment four

--- day_four/assignment/conditions.java
+++ day_four/assignment/conditions.java
@@ -5,6 +5,22 @@ public class conditions {
     public static void main(String[] args) {
         Scanner scannerObj = new Scanner(System.in);
 
-        
+        System.out.print("Enter your first number: ");
+        int numOne = scannerObj.nextInt();
+        scannerObj.nextLine();
+
+        System.out.print("Enter your seconc number: ");
+        int numTwo = scannerObj.nextInt();
+        scannerObj.nextLine();
+
+        if(numOne > numTwo) {
+            System.out.println("The larger number is: " + numOne);
+        } else if(numOne < numTwo) {
+            System.out.println("The larger number is: " + numTwo);
+        } else {
+            System.out.println("The numbers given are equal to each other.");
+        }
+
+        scannerObj.close();
     }
 }
